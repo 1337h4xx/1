379128f6e28d314dd05672f8d33e1a0c0630a392
h4xor <161542215+1337h4xx@users.noreply.github.com>
2024-03-18 12:52:56 -0700

    Update Head_T_Esp_BigHead.lua

--- Head_T_Esp_BigHead.lua
+++ Head_T_Esp_BigHead.lua
@@ -16,7 +16,7 @@ end
 local function toggleHeadSize()
     toggle = not toggle -- switch the toggle state
     if toggle then -- if toggle is true
-        headSize = 8 -- set head size
+        headSize = 4 -- set head size
     else -- if toggle is false
         headSize = 13 -- set head size
     end
