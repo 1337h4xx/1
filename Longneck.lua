5ea8650ea22b4edea2a4d5e4957eb287e044b0d9
h4xor <161542215+1337h4xx@users.noreply.github.com>
2024-03-25 22:03:01 -0700

    Update Longneck.lua

--- Longneck.lua
+++ Longneck.lua
@@ -36,40 +36,8 @@ game:GetService("UserInputService").InputBegan:Connect(function(input)
     end
 end)
 
-local longneck = { LongNeckEnabled = false, UpperLimitDefault = -8.59, LowerLimitDefault = -8.59, }
-local keybind = 'O'
-local function toggleLongNeck()
-    longneck.LongNeckEnabled = not longneck.LongNeckEnabled
-    if not longneck.LongNeckEnabled then
-        game:GetService("Workspace").Ignore.LocalCharacter.Bottom.PrismaticConstraint.UpperLimit = longneck.UpperLimitDefault
-        game:GetService("Workspace").Ignore.LocalCharacter.Bottom.PrismaticConstraint.LowerLimit = longneck.LowerLimitDefault
-    else
-        game:GetService("Workspace").Ignore.LocalCharacter.Bottom.PrismaticConstraint.UpperLimit = 4
-        game:GetService("Workspace").Ignore.LocalCharacter.Bottom.PrismaticConstraint.LowerLimit = 1.75
-    end
-end
-
-game:GetService("UserInputService").InputBegan:Connect(function(input)
-    if input.KeyCode == Enum.KeyCode[keybind] then
-        toggleLongNeck()
-    end
-end)
-
-local longneck = { LongNeckEnabled = false, UpperLimitDefault = -2.90, LowerLimitDefault = -2.90, }
-local keybind = 'K'
-local function toggleLongNeck()
-    longneck.LongNeckEnabled = not longneck.LongNeckEnabled
-    if not longneck.LongNeckEnabled then
-        game:GetService("Workspace").Ignore.LocalCharacter.Bottom.PrismaticConstraint.UpperLimit = longneck.UpperLimitDefault
-        game:GetService("Workspace").Ignore.LocalCharacter.Bottom.PrismaticConstraint.LowerLimit = longneck.LowerLimitDefault
-    else
-        game:GetService("Workspace").Ignore.LocalCharacter.Bottom.PrismaticConstraint.UpperLimit = 4
-        game:GetService("Workspace").Ignore.LocalCharacter.Bottom.PrismaticConstraint.LowerLimit = 1.75
-    end
-end
-
-local longneck = { LongNeckEnabled = false, UpperLimitDefault = -3.50, LowerLimitDefault = -3.50, }
-local keybind = 'L'
+local longneck = { LongNeckEnabled = false, UpperLimitDefault = 6.5, LowerLimitDefault = 6.5, }
+local keybind = 'F'
 local function toggleLongNeck()
     longneck.LongNeckEnabled = not longneck.LongNeckEnabled
     if not longneck.LongNeckEnabled then
