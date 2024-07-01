7dee4e1734c0024d3432bb550b8d5091c461fb18
h4xor <161542215+1337h4xx@users.noreply.github.com>
2024-07-01 07:54:04 -0700

    Create zoom.lua

---
+++ zoom.lua
@@ -0,0 +1,14 @@
+local uis = game:GetService("UserInputService")
+-- u can change Enum.KeyCode.V to the key u want for example if u want it to be B u change it to Enum.KeyCode.B
+
+uis.InputBegan:Connect(function(a)
+    if a.KeyCode == Enum.KeyCode.Q then
+        workspace.CurrentCamera.FieldOfView = 10
+    end
+end)
+
+uis.InputEnded:Connect(function(b)
+    if b.KeyCode == Enum.KeyCode.Q then
+        workspace.CurrentCamera.FieldOfView = 60
+    end
+end)
