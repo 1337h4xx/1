9e8c48cf72b3278cff8740f97b8dc8cff2537766
h4xor <161542215+1337h4xx@users.noreply.github.com>
2024-07-06 02:23:38 -0700

    Delete disable grass.lua

--- disable grass.lua
+++
@@ -1,8 +0,0 @@
--- Function to disable grass rendering
-local function DisableGrassRendering()
-    -- Assuming sethiddenproperty is a valid function
-    sethiddenproperty(game:GetService("Workspace").Terrain, "Decoration", false)
-end
-
--- Call the DisableGrassRendering function when needed, such as when the game starts
-DisableGrassRendering()
